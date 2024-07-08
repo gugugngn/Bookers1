6bebe6826d12a89f8cfacb0348f75e72ff7eb97a
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-07-08 09:05:27 +0000

    [Finish]bookのupdate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,47 @@
 class BooksController < ApplicationController
+
   def new
-    
+   
   end
   
   def index
-  
+   @books = Book.all
+  # 一覧画面には投稿したbook全てを表示させるので、全てのデータを取得する記述
+   @book = Book.new
+  # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成??はて。。。？
   end
   
-  def show
-    
-  end
-  
+   def create
+  book = Book.new(book_params)
+  # データを受け取り新規登録するためのインスタンス作成
+  book.save
+  redirect_to book_path(book.id)
+  #show.html.erbを表示
+   end 
  
+  def show
+   @book = Book.find(params[:id])
+  end
+  
+  def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
+  end
+  
+  def destroy
+   
+  end
+ 
+ private
+ def book_params
+   params.require(:book).permit(:title, :body)
+  # bookモデルを指定し、データを絞り込み⇨保存するカラムを指定。脆弱性を防いでいるらしい。
+ end 
 end
+
+ 
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
   def top
+      @book = Book.new
+    # Book .new:Bookモデルに格納されているtitleとbodyカラムを格納、＠で@をつけることでコントローラーとViewでデータの受け渡しができる
   end
 end
